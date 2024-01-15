7b014847cb4161cadafedd78381fc04eb928a5fc
Bonevy BEBY <bonevybeby@gmail.com>
2024-01-15 10:35:27 +0100

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 //import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Products from './components/Products';
 
 function App() {
   return (
-
-    <h1>
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home />}></Route>
+        <Route path="/Products" element={<Products />}></Route>
+      </Routes>
+    </BrowserRouter>
 
   );
 }
